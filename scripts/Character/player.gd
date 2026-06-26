d3f09c495e189a01f2891c633a69940b275ff68e
CBKain <sterlingford5@gmail.com>
2026-06-26 18:08:19 -0500

    Moved all the player movement code to the player controller component

--- scripts/Character/player.gd
+++ scripts/Character/player.gd
@@ -1,45 +1,15 @@
 extends CharacterBody2D
 
-##Character's max movement speed
-@export var move_speed : float = 300
-
-##How fast the character gets to max speed
-@export var acceleration : float = 20
-@export var braking : float = 20
-@export var jump_force : float = 400
-@export var double_jump_count : int = 1
-var current_double_jump : int = 0
-@export_range(0.1, 1.0) var double_jump_per : float = 0.5
-var double_jump : float = -jump_force * double_jump_per
 
 @onready var sprite : AnimatedSprite2D = %PlayerSprite
 @onready var controller := %PlayerController
 
-var move_input : float
+
 
 func _physics_process(delta: float) -> void:
+	pass
 	
-	if is_on_floor():
-		current_double_jump = 0
-	
-	#player_movement(delta)
-	
-	move_and_slide()
 
-func player_movement(delta: float):
-	
-	move_input = Input.get_axis("move_left","move_right")
-	
-	if move_input != 0:
-		velocity.x = lerp(velocity.x, move_input * move_speed, acceleration * delta)
-	else:
-		velocity.x = lerp(velocity.x, 0.0, braking * delta)
-	
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = -jump_force
-		#if Input.is_action_just_pressed("jump") and not is_on_floor() and current_double_jump < double_jump_count:
-			#velocity.y = double_jump
-		
 
 func _process(delta: float) -> void:
 	
--- scripts/Component/player_controller.gd
+++ scripts/Component/player_controller.gd
@@ -5,14 +5,23 @@ extends Node
 @export var acceleration : float = 20.0
 @export var braking : float = 20.0
 @export var jump_force : float = -400.0
+@export var multi_jump_count : int = 1
+@export_range(0.1, 1.5) var multi_jump_per : float = 0.5
+
 var move_input : float
+var current_multi_jump_count : int = 0
+var multi_jump : float = jump_force * multi_jump_per
 
 func _physics_process(delta: float) -> void:
-	player_movement(delta)
 	
+	player_movement(delta)
+	body.move_and_slide()
 
 
 func player_movement(delta: float):
+	
+	if body.is_on_floor():
+		current_multi_jump_count = 0
 	
 	move_input = Input.get_axis("move_left", "move_right")
 	
